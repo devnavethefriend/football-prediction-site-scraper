2ddae0cda71aee30c30e3f7d31dbc8ad29ae3ed4
Austine Micheal <evanzmickey@gmail.com>
2023-03-01 17:58:30 +0100

    added a bit of documentation.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+# Import required module
 from bs4 import BeautifulSoup
 import requests
 
+# Getting website using requests & initiating soup instance
 html_text = requests.get('https://www.forebet.com/en/football-tips-and-predictions-for-today/predictions-under-over-goals').text
 soup = BeautifulSoup(html_text, 'lxml')
+
+# Creating var containers for soup objects using element tags
 highest_probabilities = soup.find_all('span', class_='fpr')
 predictions = soup.find_all('span', class_='forepr')
 homeTeams = soup.find_all('span', class_='homeTeam')
@@ -10,6 +14,7 @@ awayTeams = soup.find_all('span', class_='awayTeam')
 container_A = soup.find_all('div', class_='rcnt tr_0')
 container_B = soup.find_all('div', class_='rcnt tr_1')
 
+# This code joins two seperate list containers into one
 list_of_games = []
 list_of_games.extend(container_A)
 list_of_games.extend(container_B)
