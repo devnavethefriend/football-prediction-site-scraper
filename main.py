d6ee71c73ef4688364ccb2ef667a07ff83097c14
Austine Micheal <evanzmickey@gmail.com>
2023-03-01 18:27:13 +0100

    added in new code & documentation

--- main.py
+++ main.py
@@ -14,9 +14,19 @@ awayTeams = soup.find_all('span', class_='awayTeam')
 container_A = soup.find_all('div', class_='rcnt tr_0')
 container_B = soup.find_all('div', class_='rcnt tr_1')
 
-# This code joins two seperate list containers into one
+# This code joins two seperate list containers into one & extracts the first 10 items
 list_of_games = []
 list_of_games.extend(container_A)
 list_of_games.extend(container_B)
-print(len(list_of_games))
+first_ten_games = list_of_games[:10]
+print(len(first_ten_games))
+
+high_odd_list = [i.txt for i in highest_probabilities]
+first_ten_odds = high_odd_list[:10]
+print("done")
+print(len(first_ten_odds))
+
+dict_games = zip(first_ten_games, first_ten_odds)
+
+
 
