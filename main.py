59446edb21993b16392cbfe5efc13d06b9a303ca
Austine Micheal <evanzmickey@gmail.com>
2023-03-05 15:38:57 +0100

    modified main.py

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
-# Import required module
+# Import beautifulsoup & request module
 from bs4 import BeautifulSoup
 import requests
 
-# Getting website using requests & initiating soup instance
+# Getting website URL & initiating soup instance
 html_text = requests.get('https://www.forebet.com/en/football-tips-and-predictions-for-today/predictions-under-over-goals').text
 soup = BeautifulSoup(html_text, 'lxml')
 
 # Creating var containers for soup objects using element tags
 highest_probabilities = soup.find_all('span', class_='fpr')
 predictions = soup.find_all('span', class_='forepr')
-homeTeams = soup.find_all('span', class_='homeTeam')
-awayTeams = soup.find_all('span', class_='awayTeam')
-container_A = soup.find_all('div', class_='rcnt tr_0')
-container_B = soup.find_all('div', class_='rcnt tr_1')
+home_teams = soup.find_all('span', class_='homeTeam')
+away_teams = soup.find_all('span', class_='awayTeam')
 
-# This code joins two seperate list containers into one & extracts the first 10 items
-list_of_games = []
-list_of_games.extend(container_A)
-list_of_games.extend(container_B)
-first_ten_games = list_of_games[:10]
-print(len(first_ten_games))
+# Get first ten matches & predictions
+pred_list = predictions[:10]
+prob_list = highest_probabilities[:10]
+home_list = home_teams[:10]
+away_list = away_teams[:10]
 
-high_odd_list = [i.txt for i in highest_probabilities]
-first_ten_odds = high_odd_list[:10]
-print("done")
-print(len(first_ten_odds))
+print("Done retrieving data")
+print("Printing teams and predictions")
 
-dict_games = zip(first_ten_games, first_ten_odds)
-
-
-
+# Display each match & prediction
+for prob, hometeam, awayteam, predict in zip(prob_list, home_list, away_list, pred_list):
+    print(f"{hometeam.text} vs {awayteam.text}\n Probability: {prob.text}\n Prediction: {predict.text}")
