d2ecdb53dfe0f74e1a1b93c256e072c0fee57007
Austine Micheal <evanzmickey@gmail.com>
2023-03-06 16:09:36 +0100

    retouch of initial code.

--- main.py
+++ main.py
@@ -1,26 +1,43 @@
-# Import beautifulsoup & request module
+# import beautifulsoup & request module
 from bs4 import BeautifulSoup
 import requests
 
-# Getting website URL & initiating soup instance
-html_text = requests.get('https://www.forebet.com/en/football-tips-and-predictions-for-today/predictions-under-over-goals').text
-soup = BeautifulSoup(html_text, 'lxml')
+# make a request to the website
+url = "https://www.forebet.com/en/football-tips-and-predictions-for-today/predictions-under-over-goals"
+response = requests.get(url)
 
-# Creating var containers for soup objects using element tags
+# parse the HTML content using BeautifulSoup
+soup = BeautifulSoup(response.content, "html.parser")
+
+# find the sections containing the teams & predictions
 highest_probabilities = soup.find_all('span', class_='fpr')
 predictions = soup.find_all('span', class_='forepr')
 home_teams = soup.find_all('span', class_='homeTeam')
 away_teams = soup.find_all('span', class_='awayTeam')
 
-# Get first ten matches & predictions
-pred_list = predictions[:10]
-prob_list = highest_probabilities[:10]
-home_list = home_teams[:10]
-away_list = away_teams[:10]
+# define a function to get the matches & predictions for a given number of inputs
+def get_matches_and_predictions(user_input):
+    pred_list = predictions[:user_input]
+    prob_list = highest_probabilities[:user_input]
+    home_list = home_teams[:user_input]
+    away_list = away_teams[:user_input]
 
-print("Done retrieving data")
-print("Printing teams and predictions")
+    return (pred_list, prob_list, home_list, away_list)
 
-# Display each match & prediction
-for prob, hometeam, awayteam, predict in zip(prob_list, home_list, away_list, pred_list):
-    print(f"{hometeam.text} vs {awayteam.text}\n Probability: {prob.text}\n Prediction: {predict.text}")
+# prompt the user for the number of matches to get
+try:
+    # make sure input is integer
+    user_input = int(input("Enter the number of matches to retrieve: "))
+    print("Loading information...")
+except ValueError:
+    # catch the ValueError exception if the input is not an integer
+    print("Input error!")
+
+# get the first 'user_input' matches and predictions
+matches_and_predictions = get_matches_and_predictions(user_input)
+
+# open a new text file for writing
+with open("result.txt", "w") as file:
+    # write each match & prediction
+    for prob, hometeam, awayteam, predict in matches_and_predictions:
+        file.write(f"{hometeam.text} vs {awayteam.text}  Probability: {prob.text}  Prediction: {predict.text}\n")
